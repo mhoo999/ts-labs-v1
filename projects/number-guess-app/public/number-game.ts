dfa42a930b2bf1394ceea8af3bc5885374e5cc61
myeonghoon <mhoo999@naver.com>
2025-06-01 19:00:31 +0900

    number-guess-app

---
+++ projects/number-guess-app/public/number-game.ts
@@ -0,0 +1,57 @@
+const target: number = Math.floor(Math.random() * 100);
+console.log(target);
+let count: number = 0;
+
+const guessForm = document.getElementById("guess-form") as HTMLFormElement;
+const guessInput = document.getElementById("guess-input") as HTMLFormElement;
+const trialCount = document.getElementById("trial-count");
+const resultMsg = document.getElementById("result-msg");
+const resetBtn = document.getElementById("reset-btn");
+const historyList = document.getElementById("history-list");
+
+guessForm?.addEventListener("submit", checkNumber);
+
+function checkNumber(event: Event) {
+    event.preventDefault();
+
+    const userGuess = parseInt(guessInput.value);
+
+    count++;
+    trialCount!.textContent = `시도: ${count}회`
+
+    const li = document.createElement("LI");
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+
+    console.log(userGuess);
+    if (userGuess > target) {
+        resultMsg!.textContent = "Down!";
+        span1.textContent = guessInput.value;
+        span2.textContent = "Down!";
+
+    } else if (userGuess < target) {
+        resultMsg!.textContent = "Up!";
+        span1.textContent = guessInput.value;
+        span2.textContent = "Up!";
+
+    } else {
+        resultMsg!.textContent = `정답입니다! 시도 횟수: ${count}회`;
+        span1.textContent = guessInput.value;
+        span2.textContent = "정답!";
+
+        resetBtn!.style.display = "block";
+        resultMsg!.style.color = "rgb(54, 135, 38)";
+        guessInput.disabled = "true";
+        
+    }
+
+    li.append(span1, span2);
+    historyList!.append(li);
+
+    guessInput.value = "";
+    guessInput.focus();
+}
+
+resetBtn?.addEventListener("click", () => {
+    location.reload();
+})
